c07cdcf86b1484e97a22b802941876d827fc9329
wegfawefgawefg <668es218pur@gmail.com>
2017-01-11 13:39:08 -0600

    -this actually works now

--- 2_nn.py
+++ 2_nn.py
@@ -11,8 +11,8 @@ import numpy
 '''
 
 #	forward propogation
-def forwardGate( x, y ):
-	return x * y 
+def forwardGate( inp, weight ):
+	return ( inp * weight ) 
 
 #	calculate derivative
 def calcDerivative( x, y, h ):
@@ -23,103 +23,108 @@ def calcDerivative( x, y, h ):
 	return derivative
 
 #	calculate derivative on error
-def calcErrorDerivative( x, y, h, target ):
-	output_0 = forwardGate( x 		, y )
-	error_0 = target - output_0
+def calcErrorDerivative( inp, weight, h, target ):
+	output_0 = forwardGate( inp 	, weight 	)
+	error_0 = target - output_0 
 
-	output_1 = forwardGate( x + h 	, y )
+	output_1 = forwardGate( inp 	, weight + h )
 	error_1 = target - output_1
 
 	derivative = ( error_1 - error_0 ) / h
 	return derivative
 
+
 #	outputs a randomy number between -1.0 and 1.0
 def randy():
 	return ( random.random() * 2.0 ) - 1.0
+
+#	returns sign of a number
+def sign( a ):
+	if a >= 0:
+		return 1
+	if a < 0:
+		return -1
 
 '''	goal is to make output equal to a target
 	
 	-currently no complex backprop as only one neuron present
 	-not sure if derivative is even right
-	-why the fuck do the weights go up to infinity
+	-doesnt like negative targets
+		|-suspect this has to do with an incorrect sign somewhere
 '''
 
 #	-	-	-	-	-	SETTINGS	-	-	-	-	-	#
-target = 500.0
-tries = 50
+target = random.random() * 100.0
+tries = 100
 stepSize = 0.1
 h = 0.0001
 
-x = 1.0
-y = 1.0
+inp = 2.0
+weight = random.random()
 
 #	-	-	-	-	-	PREP	-	-	-	-	-	#
 output = 0.0
 error = 0.0
-lastError = 0.0
 
-output = forwardGate( x, y )
-error = output - target
-bestError = error
+output = forwardGate( inp, weight )
+error = target - output
 
 #	save initial value
-initialY = y
-initialX = x
+initialWeight = weight
 initialOutput = output
 initialError = error
 
 #	-	-	-	-	-	PREP NUMPY 	-	-	-	-	-	#
 xAxisValues = range( 0, tries + 1 )
-yAxisValues = [ bestError ]
+yAxisValues = [ initialError ]
 
 #	-	-	-	-	-	MAIN	-	-	-	-	-	#
 
 for i in range( 0, tries ):
 
 	#	calculate error
-	output = forwardGate( x, y )
-	error = output - target
+	output = forwardGate( inp, weight )
+	error = target - output
 
 	#	calculate derivative
-	xDir = calcErrorDerivative( x, y, h, target )
-	yDir = calcErrorDerivative( y, x, h, target )
+	wDir = calcErrorDerivative( inp, weight, h, target )
 
 	#	DEBUG
-	print("( xDir, yDir ): " + str(xDir) + ", " + str(yDir) )
+	#print("wDir:\t" + "%.2f" % wDir + "\tError:\t" + "%.2f" % error )
 
 	#	tweak x and y in direction of derivative
-	x = x + stepSize * -xDir * error
-	y = y + stepSize * -yDir * error
+	weight = weight + ( error / -wDir ) * stepSize
 
 	#	STATS:
 	##	add current best error to yAxisValues for graphing
-	yAxisValues.append(error)
+	yAxisValues.append( error )
 
 #	-	-	-	-	-	DATA COLLECTING	-	-	-	-	-	#
 #	save final value
-finalY = y
-finalX = x
+finalWeight = weight
 
-output = forwardGate( finalX, finalY )
-error = output - target
+output = forwardGate( inp, finalWeight )
+error = target - output
 
-bestOutput = output
-bestError = error
+finalOutput = output
+finalError = error
 
 
 #	-	-	-	-	-	RESULTS	-	-	-	-	-	#
+print( "#	-	-	-	RESULTS	-	-	-	#" )
+
 #	output state before changes
-print( "x was " + str( initialX ) )
-print( "y was " + str( initialY ) )
-print( "output was: " + str( initialOutput ) )
-print( "error was: " + str( initialError ) )
+print( "weight 	was: \t" 	+ str( initialWeight 	) )
+print( "output 	was: \t" 	+ str( initialOutput 	) )
+print( "target 	was: \t" 	+ str( target 	 		) )
+print( "error 	was: \t" 	+ str( initialError  	) )
 print("")
 
 #	output state after changes
-print( "x is " + str( finalX ) )
-print( "y is " + str( finalY ) )
-print( "output: " + str( bestOutput ) )
-print( "error: " + str( bestError ) )
+print( "weight 	is: \t" 	+ str( finalWeight 	) )
+print( "output 	is: \t" 	+ str( finalOutput 	) )
+print( "target 	is: \t" 	+ str( target 		) )
+print( "error 	is: \t"		+ str( finalError 	) )
 
 #	-	-	-	-	-	PLOT 	-	-	-	-	-	#
 
